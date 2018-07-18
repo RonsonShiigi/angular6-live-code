80dd75ae9128bbe9c7ea9cff09f4248c51806800
Edward Kim <taesup63@gmail.com>
2018-07-17 14:58:58 -1000

    Added more pages and a router
    Added an about page and a contact page. The contact page has a form
    that is bound to an object in contact.component.ts that is also
    bound to the form using two way data binding. The FormsModule was
    also added to the app.component.module file.
    
    The router can load all three pages and has a catch all for anything
    else. The RouterModule was also added to the app.component.module
    file. The <router-outlet> component was also added to the html file
    for the main app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // for input handling
+import { RouterModule } from '@angular/router'; // for routing
+import { BrowserModule } from '@angular/platform-browser';
 
+// Components
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+
+// Pages
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      [
+        { path: '', component: HomeComponent },
+        { path: 'contact', component: ContactComponent },
+        { path: 'about', component: AboutComponent },
+        { path: '**', redirectTo: '', pathMatch: 'full' }
+      ],
+      { enableTracing: true } // this is for debugging only
+    ),
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
