ddab17daa154b6535e4acd8ada2cb7a1bba4b4e3
Edward Kim <taesup63@gmail.com>
2018-07-17 15:43:14 -1000

    Component Communication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,26 +6,30 @@ import { BrowserModule } from '@angular/platform-browser';
 // Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ParentComponent } from './components/parent/parent.component';
+import { ChildComponent } from './components/child/child.component';
 
 // Pages
 import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { CommunicationComponent } from './pages/communication/communication.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutComponent,
-    ContactComponent
+    ContactComponent,
+    CommunicationComponent,
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
     RouterModule.forRoot(
       [
         { path: '', component: HomeComponent },
         { path: 'contact', component: ContactComponent },
-        { path: 'about', component: AboutComponent },
+        { path: 'communication', component: CommunicationComponent },
         { path: '**', redirectTo: '', pathMatch: 'full' }
       ],
       { enableTracing: true } // this is for debugging only
